a488f9ada9a5c0bcf9622f8c71737ea2f928e7be
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-06-20 23:57:57 -0600

    Able to move player horizontally and keep response to gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+  [SerializeField] float runSpeed = 10f;
   Vector2 moveInput;
+  Rigidbody2D rb2D;
 
   void Start()
   {
-
+    rb2D = GetComponent<Rigidbody2D>();
   }
 
 
   void Update()
   {
-
+    Run();
   }
 
   void OnMove(InputValue value)
@@ -23,4 +25,11 @@ public class PlayerMovement : MonoBehaviour
     moveInput = value.Get<Vector2>();
     Debug.Log(moveInput);
   }
+
+  void Run()
+  {
+    Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb2D.velocity.y);
+    rb2D.velocity = playerVelocity;
+  }
+
 }
