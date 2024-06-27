06dc868b274250e639b335f6e0ec85542d2cb648
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-06-27 14:18:12 -0600

    Coin Audio

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+  [SerializeField] AudioClip coinAudio;
+
   void OnTriggerEnter2D(Collider2D other)
   {
     if (other.tag == "Player")
     {
+      AudioSource.PlayClipAtPoint(coinAudio, Camera.main.transform.position);
       Destroy(gameObject);
     }
   }
