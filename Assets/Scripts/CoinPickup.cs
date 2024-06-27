7ae4bf2edc9d74b011bd3ca44ea4d5b2748ac270
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-06-27 14:43:21 -0600

    Avoid potential double pickup bug

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -7,12 +7,15 @@ public class CoinPickup : MonoBehaviour
   [SerializeField] AudioClip coinAudio;
   [SerializeField] int scoreValue = 100;
 
+  bool wasCollected = false;
+
   void OnTriggerEnter2D(Collider2D other)
   {
-    if (other.tag == "Player")
+    if (other.tag == "Player" && !wasCollected)
     {
       AudioSource.PlayClipAtPoint(coinAudio, Camera.main.transform.position);
       FindObjectOfType<GameSession>().IncreaseScore(scoreValue);
+      gameObject.SetActive(false);
       Destroy(gameObject);
     }
   }
