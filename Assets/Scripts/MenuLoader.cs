e0ef70cf10e45e3700c084332f25ef675d0afd33
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-10-14 13:16:34 -0600

    Added UI sound effect for buttons

--- Assets/Scripts/MenuLoader.cs
+++ Assets/Scripts/MenuLoader.cs
@@ -5,14 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuLoader : MonoBehaviour
 {
+  [SerializeField] AudioClip selectionAudio;
+
   public void StartGame()
   {
+    AudioSource.PlayClipAtPoint(selectionAudio, Camera.main.transform.position);
     SceneManager.LoadScene(1);
   }
 
   public void ExitGame()
   {
     Debug.Log("Qutting game.");
+    AudioSource.PlayClipAtPoint(selectionAudio, Camera.main.transform.position);
     Application.Quit();
   }
 }
