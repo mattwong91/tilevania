21745de1b1e9b539a9197418593a2c9fdeeb889a
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-11-14 14:28:47 -0700

    Parallax background

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+  [SerializeField] Vector2 moveSpeed;
+  [SerializeField] bool scrollLeft;
+  Vector3 offset;
+
+  float singleTextureWidth;
+
+  void Start()
+  {
+    if (scrollLeft) { moveSpeed = -moveSpeed; }
+    Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+    singleTextureWidth = sprite.texture.width / sprite.pixelsPerUnit;
+  }
+
+  void Update()
+  {
+    Scroll();
+    ResetPosition();
+  }
+
+  void Scroll()
+  {
+    offset = moveSpeed * Time.deltaTime;
+    transform.position += offset;
+  }
+
+  void ResetPosition()
+  {
+    if (Math.Abs(transform.position.x / transform.localScale.x) - singleTextureWidth > 0)
+    {
+      transform.position = new Vector3(0, transform.position.y, transform.position.z);
+    }
+  }
+}
